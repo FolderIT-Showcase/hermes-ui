0548c36a022a7b1014b358546aaf8e0d3da057a2
Daniel Campodonico <dcampodonico@folderit.net>
2017-10-05 12:36:50 -0300

    Modelo punto venta

---
+++ src/app/shared/domain/puntoVenta.ts
@@ -0,0 +1,8 @@
+import {Async, Decimal, Enum, MaxLength, Min, Required} from '../services/annotations';
+
+export class PuntoVenta {
+  @Required @Async('puntosventa') @Decimal(4, 0) @Min(0) id: number;
+  @Required @Enum(['IMP', 'IF', 'FE'], ['Impresión Común', 'Impresora Fiscal', 'Facturación Electrónica']) tipo_impresion: string;
+  habilitado: Boolean = true;
+  @MaxLength(191) descripcion: string;
+}
